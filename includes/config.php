1bd480b69d292d58796b3cf52cce9b23304c3a48
Thalita <telessthalita@gmail.com>
2025-04-28 18:36:56 -0300

    feat: config do banco

---
+++ includes/config.php
@@ -0,0 +1,15 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'alfama_web');
+
+session_start();
+
+try {
+    $pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("ERRO: Não foi possível conectar. " . $e->getMessage());
+}
+?>
